9424dba2f9309633f1b1ffc08caad4c87d39dd79
Matias Hettich Castillo <matiashettich@outlook.com>
2024-02-13 19:59:31 -0300

    added com player

---
+++ scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+enum OPTIONS {
+	P1_VS_COM,
+	P1_VS_P2
+}
+var current_option = OPTIONS.P1_VS_COM
--- scripts/player_2_score_value_controller.gd
+++ scripts/player_2_score_value_controller.gd
@@ -1,4 +1,13 @@
 extends Label
 
+func _ready():
+	if global.current_option == global.OPTIONS.P1_VS_P2:
+		text = "P2 Score: 0"
+	else:
+		text = "COM Score: 0"
+
 func _on_main_update_player_2_score(value):
-	text = "P2 Score: " + str(value)
+	if global.current_option == global.OPTIONS.P1_VS_P2:
+		text = "P2 Score: " + str(value)
+	else:
+		text = "COM Score: " + str(value)
